becc2710c8524eb953824d13b5571e1b6119e75b
xiaomalover <xiaomalover@gmail.com>
2018-05-23 14:05:42 +0800

    update

--- src/main/java/com/weison/exception/GlobalExceptionHandler.java
+++ src/main/java/com/weison/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -14,7 +13,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
         ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", e);
+        mav.addObject("exception", e.getMessage());
         mav.addObject("url", req.getRequestURL());
         mav.setViewName("error");
         return mav;
